e0a5a7524b6d65bb44ad05225271d46f6f49ca45
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2025-02-27 05:26:03 +0700

    add db connect and adjust route handler

--- api/router.go
+++ api/router.go
@@ -1,18 +1,34 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"go-spe/internal/auth"
+	"go-spe/internal/domain/repository"
+	"go-spe/internal/domain/service"
+	"go-spe/pkg/db"
+	"go-spe/pkg/handler"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
+	dbConn, err := db.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect to the database: ", err)
+	}
+
 	v1 := router.Group("/api/v1")
 	{
+		// Initialize TransactionHandler
+		transactionRepo := repository.NewTransactionRepositor(dbConn)
+		transactionService := service.NewTransactionService(transactionRepo)
+		transactionHandler := handler.NewTransactionHandler(transactionService)
+
 		v1.GET("/", auth.SignatureMiddleware(), func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"message": "Welcome to API"})
 		})
+		v1.POST("/check-status", auth.SignatureMiddleware(), transactionHandler.CheckTransactionStatus)
 	}
 }
